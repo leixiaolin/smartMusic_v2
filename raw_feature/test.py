e2b10fd240089ef0a01d177ea8cc26eeb1196182
e <498900619@qq.com>
2019-03-12 22:47:45 +0800

    添加旋律型的训练模型； 显示判别出错的具体类型。

--- raw_feature/test.py
+++ raw_feature/test.py
@@ -1,25 +1,20 @@
-import cv2
-import numpy as np
-original_img = cv2.imread('f:/2.png',0)
-# gray_res = cv2.resize(original_img,None,fx=1,fy=1,
-#                  interpolation = cv2.INTER_CUBIC)                #图形太大了缩小一点
-# B, G, img = cv2.split(res)
-# _,RedThresh = cv2.threshold(img,160,255,cv2.THRESH_BINARY)     #设定红色通道阈值160（阈值影响开闭运算效果）
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(20,20))         #定义矩形结构元素
+import crepe
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import librosa
 
-closed1 = cv2.morphologyEx(original_img, cv2.MORPH_CLOSE, kernel,iterations=1)    #闭运算1
-closed2 = cv2.morphologyEx(original_img, cv2.MORPH_CLOSE, kernel,iterations=3)    #闭运算2
-opened1 = cv2.morphologyEx(original_img, cv2.MORPH_OPEN, kernel,iterations=1)     #开运算1
-opened2 = cv2.morphologyEx(original_img, cv2.MORPH_OPEN, kernel,iterations=3)     #开运算2
-gradient = cv2.morphologyEx(original_img, cv2.MORPH_GRADIENT, kernel)             #梯度
 
-#显示如下腐蚀后的图像
-#cv2.imshow("gray_res", gray_res)
-cv2.imshow("Close1",closed1)
-#cv2.imshow("Close2",closed2)
-#cv2.imshow("Open1", opened1)
-#cv2.imshow("Open2", opened2)
-#cv2.imshow("gradient", gradient)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/节奏/节奏8_40434（30）.wav'
+filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/视唱1-01（95）.wav'
+filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/视唱1-02（90）.wav'
+filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/旋律1.1(95).wav'
+filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/旋律2（四）(96).wav'
+filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/旋律2卢(98).wav'
+#filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/旋律2.1(80).wav'
+sr, audio = wavfile.read(filename)
+time = librosa.get_duration(filename=filename)
+print(time)
+time, frequency, confidence, activation = crepe.predict(audio, sr,step_size=10, viterbi=True)
+plt.plot(time,frequency)
+plt.show()
+print(frequency)
