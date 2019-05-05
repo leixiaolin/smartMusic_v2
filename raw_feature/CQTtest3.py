25b29684b5f5eb5929fc7508ad7d39182cc88fe2
e <498900619@qq.com>
2019-05-05 23:23:39 +0800

    添加旋律型的训练模型； 显示判别出错的具体类型。

--- raw_feature/CQTtest3.py
+++ raw_feature/CQTtest3.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import librosa.display
 import numpy as np
 from create_base import *
+from myDtw import *
+from find_mismatch import *
 # 1. Get the file path to the included audio example
 # Sonify detected beat events
 # 定义加载语音文件并去掉两端静音的函数
@@ -15,7 +17,20 @@ def load_and_trim(path):
 
     return audio, sr
 
-def get_note_line_start_v2(cqt):
+def get_note_line_start_v2(cqt,rhythm_codes):
+    small_code_indexs = [i for i in range(len(rhythm_codes)) if rhythm_codes[i] == '250']
+    threshold_length = 4
+    threshold_length_before = 4
+    threshold_length_after = 4
+    if len(small_code_indexs) < 1:
+        threshold_length = 10
+    else:
+        less_half = [i for i in range(len(small_code_indexs)) if small_code_indexs[i] < int(len(rhythm_codes)/2) ]
+        more_half = [i for i in range(len(small_code_indexs)) if small_code_indexs[i] > int(len(rhythm_codes) / 2)]
+        if len(less_half) < 1:
+            threshold_length_before = 10
+        if len(more_half) < 1:
+            threshold_length_after = 10
     min_cqt = np.min(cqt)
     max_cqt = np.max(cqt)
     w,h = cqt.shape
@@ -44,13 +59,54 @@ def get_note_line_start_v2(cqt):
                     offset += 1
                 else:
                     flag = False
-                    if best_longest > 4 :
+                    if col <= int(h/2):
+                        threshold_length = threshold_length_before
+                    else:
+                        threshold_length = threshold_length_after
+
+                    if best_longest > threshold_length :
                         result.append(col + best_begin)
                         end = col + best_begin + best_longest
                         end_result.append(end)
                         note_lines.append(best_row)
                         times.append(best_longest)
     return result,end_result,times,note_lines
+
+def merge_note_line(result,end_result,times,note_lines):
+    select_result = []
+    select_end_result = []
+    select_times = []
+    select_note_lines = []
+    i = 0
+    while i <= len(result)-2:
+        if result[i] + times[i] == result[i+1] and (note_lines[i] == note_lines[i+1] or np.abs(note_lines[i] - note_lines[i+1]) >=10 or np.abs(note_lines[i] - note_lines[i+1]) <=2):
+            select_result.append(result[i])
+            select_end_result.append(end_result[i+1])
+            select_times.append(times[i] + times[i+1])
+            select_note_lines.append(note_lines[i])
+            i += 2
+        else:
+            select_result.append(result[i])
+            select_end_result.append(end_result[i])
+            select_times.append(times[i])
+            select_note_lines.append(note_lines[i])
+            i += 1
+    return select_result,select_end_result,select_times,select_note_lines
+
+def merge_note_line_by_distance(result,times,note_lines):
+    select_result = []
+    select_times = []
+    select_note_lines = []
+    select_result.append(result[0])
+    select_times.append(times[0])
+    select_note_lines.append(note_lines[0])
+
+    for i in range(1,len(result)):
+        if result[i] - result[i-1] > 9 and times[i] > 5:
+            select_result.append(result[i])
+            select_times.append(times[i])
+            select_note_lines.append(note_lines[i])
+    return select_result,select_times,select_note_lines
 
 def get_longest_for_cols_cqt(cols_cqt,min_cqt):
     w,h = cols_cqt.shape
@@ -224,19 +280,13 @@ def find_loss_by_rms_mean(result,rms,CQT):
     for i in range(5,len(rms)-5):
         off = [np.abs(x -i) for x in select_result]
         min_off = np.min(off)
-        start = i - 1
+        start = i - 2
         end = i + 2
-        # 条件一：振幅有增加
-        sub_rms = [rms[start + 1] - rms[start],rms[start + 2] - rms[start],rms[start + 3] - rms[start]]
-        cond1 = np.max(sub_rms) > 0.1
+        # 条件一：跨过均值线,振幅有增加
+        cond1 = rms[start] < mean_rms_on_frames - 0.2 and rms[end] > mean_rms_on_frames + 0.2
 
-        # 条件一：跨过均值线
-        #cond2 = (rms[i] <= mean_rms_on_frames and rms[i+1]>mean_rms_on_frames) or (rms[i-1] <= mean_rms_on_frames and rms[i]>mean_rms_on_frames)
-        cond2 = rms_diff[i] > 0.3
-
-        if cond2 and min_off > 10:
+        if cond1 and min_off > 10:
             #print("np.std(sub_rms) is {}".format(np.std(sub_rms)))
-            print("np.max(sub_rms) is {}".format(np.max(sub_rms)))
             if rms[i-3]< rms[i]:
                 select_result.append(i-3)
             elif rms[i-2]< rms[i]:
@@ -251,17 +301,30 @@ def find_loss_by_rms_mean(result,rms,CQT):
     print("mean_rms_on_frames is {}".format(mean_rms_on_frames))
     return select_result
 
-def del_end_range(onset_frames,all_note_lines,rms):
+def continueUp(rms):
+    sum1, res = 0, 0
+    for i in range(len(rms)):
+        #遇1加1，遇0置0
+        sum1 = sum1*i + i
+        if sum1 > res:
+            #记录连续1的长度
+            res = sum1
+    return res
+
+def del_end_range(onset_frames,all_note_lines,times,rms):
     length = len(rms)
     select_onset_frames = []
     select_all_note_lines = []
+    select_times = []
     for i in range(len(onset_frames)):
         onset_frame = onset_frames[i]
         note_line = all_note_lines[i]
+        t = times[i]
         if onset_frame < length - 40:
             select_onset_frames.append(onset_frame)
             select_all_note_lines.append(note_line)
-    return select_onset_frames,select_all_note_lines
+            select_times.append(t)
+    return select_onset_frames,select_all_note_lines,select_times
 
 # 如果音高线以上有没有可分点
 def find_other_note_line(onset_frame,note_line,sub_cqt):
@@ -351,6 +414,7 @@ def getLongestLine(nums):
 
 def get_loss_at_begin(cqt,result,all_note_lines,longest_numbers):
     min_cqt = np.min(cqt)
+    w,h = cqt.shape
     first = result[0]
     best_longest, best_begin,best_row = 0,0,0
     if first > 60:
@@ -370,6 +434,92 @@ def get_loss_at_begin(cqt,result,all_note_lines,longest_numbers):
             longest_numbers.insert(0, best_longest)
     return result,all_note_lines,longest_numbers
 
+'''
+求两个字符串的最长公共子串
+思想：建立一个二维数组，保存连续位相同与否的状态
+'''
+
+
+def getNumofCommonSubstr(str1, str2):
+    lstr1 = len(str1)
+    lstr2 = len(str2)
+    record = [[0 for i in range(lstr2 + 1)] for j in range(lstr1 + 1)]  # 多一位
+    maxNum = 0  # 最长匹配长度
+    p = 0  # 匹配的起始位
+
+    for i in range(lstr1):
+        for j in range(lstr2):
+            if str1[i] == str2[j]:
+                # 相同则累加
+                record[i + 1][j + 1] = record[i][j] + 1
+                if record[i + 1][j + 1] > maxNum:
+                    # 获取最大匹配长度
+                    maxNum = record[i + 1][j + 1]
+                    # 记录最大匹配长度的终止位置
+                    p = i + 1
+    return str1[p - maxNum:p], maxNum
+
+def get_match_notes(diff_base_notes_str,diff_longest_note_str):
+    list_intersect,number = getNumofCommonSubstr(diff_base_notes_str, diff_longest_note_str)
+    print("diff_longest_note_str, diff_base_notes_str,intersect is {}==={}==={}".format(diff_longest_note_str, diff_base_notes_str,list_intersect))
+    return list_intersect
+
+def check_all_notes_trend(longest_note,base_notes):
+    diff_longest_note = []
+    diff_base_notes = []
+    diff_longest_note_str = ''
+    diff_base_notes_str = ''
+    for i in range(1, len(longest_note)):
+        if longest_note[i] > longest_note[i - 1]:
+            tmp = 1
+        if longest_note[i] == longest_note[i - 1]:
+            tmp = 0
+        if longest_note[i] < longest_note[i - 1]:
+            tmp = 2
+        diff_longest_note.append(str(tmp))
+        diff_longest_note_str = ''.join(diff_longest_note)
+    for i in range(1, len(base_notes)):
+        if base_notes[i] > base_notes[i - 1]:
+            tmp = 1
+        if base_notes[i] == base_notes[i - 1]:
+            tmp = 0
+        if base_notes[i] < base_notes[i - 1]:
+            tmp = 2
+        diff_base_notes.append(str(tmp))
+        diff_base_notes_str = ''.join(diff_base_notes)
+    list_intersect = get_match_notes(diff_base_notes_str,diff_longest_note_str)
+    start1 = diff_longest_note_str.find(list_intersect)
+    end1 = start1 + len(list_intersect)
+
+    start2 = diff_base_notes_str.find(list_intersect)
+    end2 = start2 + len(list_intersect)
+    list_intersect_before = []
+    list_intersect_after = []
+    if start1 >= 2 and start2 >=2:
+        list_intersect_before = get_match_notes(diff_longest_note_str[0:start1],diff_base_notes_str[0:start2])
+    if end1 >= 2 and end2 >=2:
+        list_intersect_after = get_match_notes(diff_longest_note_str[end1+1:],diff_base_notes_str[end1+1:])
+
+    return list_intersect_before,list_intersect,list_intersect_after
+
+def find_hightest_after(start,rms):
+    good_point = start
+    for i in range(start+1,len(rms)-1):
+        if rms[i] > rms[i-1]:
+            good_point += 1
+        else:
+            break
+    return good_point
+
+def change_rms(rms):
+    result = []
+    for i in range(1,len(rms)-3):
+        if rms[i+1] > rms[i] and rms[i-1] > rms[i]:
+            hightest_point_after = find_hightest_after(i, rms)
+            if rms[hightest_point_after] - rms[i] > 0.5:
+                result.append(i)
+    return result
+
 def draw_plt(filename):
     y, sr = load_and_trim(filename)
     y, sr = librosa.load(filename)
@@ -383,12 +533,32 @@ def draw_plt(filename):
     # onsets_frames = get_real_onsets_frames_rhythm(y)
     CQT = np.where(CQT > -22, np.max(CQT), np.min(CQT))
     # onsets_frames,end_position = get_note_line_start(CQT)
-    onsets_frames, end_result, times, note_lines = get_note_line_start_v2(CQT)
+    rhythm_codes = get_rhythm_codes(filename)
+    onsets_frames, end_result, times, note_lines = get_note_line_start_v2(CQT,rhythm_codes)
+    onsets_frames, end_result, times, note_lines = merge_note_line(onsets_frames, end_result, times, note_lines)
+    onsets_frames = find_loss_by_rms_mean(onsets_frames, rms, CQT)
+    onsets_frames = change_rms(rms)
     onsets_frames, note_lines, times = get_note_lines(CQT, onsets_frames)
-    print("0 onsets_frames is {}".format(onsets_frames))
-    print("0 end_result is {}".format(end_result))
-    print("0 times is {}".format(times))
-    print("0 note_lines is {}".format(note_lines))
+    onsets_frames, note_lines,times = del_end_range(onsets_frames, note_lines,times, rms)
+    onsets_frames, times, note_lines = merge_note_line_by_distance(onsets_frames, times, note_lines)
+
+    print("0 onsets_frames is {},size is {}".format(onsets_frames,len(onsets_frames)))
+    print("0 end_result is {},size is {}".format(end_result,len(end_result)))
+    print("0 times is {},size is {}".format(times,len(times)))
+    print("0 note_lines is {},size is {}".format(note_lines,len(note_lines)))
+    base_notes = base_note(filename)
+    list_intersect_before, list_intersect, list_intersect_after = check_all_notes_trend(note_lines, base_notes)
+    print("list_intersect_before,list_intersect,list_intersect_after is {},{},{}".format(list_intersect_before,
+                                                                                         list_intersect,
+                                                                                         list_intersect_after))
+
+    type_index = get_onsets_index_by_filename_rhythm(filename)
+    base_frames = onsets_base_frames_for_note(filename)
+    onset_score, lost_score, ex_score, min_d = get_score_for_note(onsets_frames.copy(), base_frames.copy(), type_index)
+    print("score, lost_score, ex_score, min_d is {},{},{},{}".format(onset_score, lost_score, ex_score, min_d))
+
+
+    score = onset_score
 
     plt.subplot(3, 1, 1)
     # librosa.display.specshow(CQT)
@@ -404,10 +574,17 @@ def draw_plt(filename):
     # plt.title('Constant-Q power spectrogram (note)')
     plt.subplot(3, 1, 2)
     times = librosa.frames_to_time(np.arange(len(rms)))
+    print("base_frames is {},size is {}".format(base_frames,len(base_frames)))
+    base_frames = [x - (base_frames[0] - onsets_frames[0]) for x in base_frames]
+    #base_frames = base_frames[3:8]
+    #min_d, best_y, _ = get_dtw_min(onsets_frames, base_frames, 100)
+    #base_times = librosa.frames_to_time(best_y, sr=sr)
+    base_times = librosa.frames_to_time(base_frames, sr=sr)
     # rms_on_onset_frames_cqt = [rms[x] for x in onset_frames_cqt]
     # min_rms_on_onset_frames_cqt = np.min(rms_on_onset_frames_cqt)
     # rms = [1 if x >=min_rms_on_onset_frames_cqt else 0 for x in rms]
     plt.vlines(onstm, 0, np.max(rms), color='y', linestyle='dashed')
+    plt.vlines(base_times, 0, np.max(rms), color='r', linestyle='dashed')
     rms_on_frames = [rms[x] for x in onsets_frames]
     mean_rms_on_frames = np.mean(rms_on_frames)
     plt.plot(times, rms)
@@ -415,14 +592,9 @@ def draw_plt(filename):
     plt.axhline(mean_rms_on_frames, color='r')
 
     plt.subplot(3, 1, 3)
-    rms = np.diff(rms)
-    rms = [0 - rms[i] if rms[i - 2] > rms[i - 1] and rms[i - 1] > rms[i] and rms[i + 1] > rms[i] and rms[i + 2] > rms[
-        i + 1] else 0 for i in range(2, len(rms) - 2)]
-    # rms = [rms[i] if rms[i+2] - rms[i] > 0.2   else 0 for i in range(2, len(rms) - 2) ]
-    rms.insert(0, 0)
-    rms.insert(1, 0)
-    rms.append(0)
-    rms.append(0)
+    rms = np.diff(rms,2)
+
+
     times = librosa.frames_to_time(np.arange(len(rms)))
     # rms_on_onset_frames_cqt = [rms[x] for x in onset_frames_cqt]
     # min_rms_on_onset_frames_cqt = np.min(rms_on_onset_frames_cqt)
@@ -433,28 +605,8 @@ def draw_plt(filename):
     plt.axhline(mean_rms_on_frames, color='r')
     plt.vlines(onstm, 0, np.max(rms), color='y', linestyle='dashed')
     plt.xlim(0, np.max(times))
-    return plt
+    return plt,score
 
-#y, sr = load_and_trim('F:/项目/花城音乐项目/样式数据/ALL/旋律/1.31MP3/旋律1.100分.wav')
-filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/旋律2.1(80).wav'
-filename = 'F:/项目/花城音乐项目/样式数据/ALL/旋律/1.31MP3/旋律3.100分.wav'
-filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/旋律一（9）（100）.wav'
-#filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/旋律一（14）（95）.wav'
-#filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/节奏/节2罗（75）.wav'
-#filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/节奏/节奏一（4）（96）.wav'
-filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋2熙(0).wav'
-filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋1罗（96）.wav'
-filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋10罗（92）.wav'
-#filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋2录音4(72).wav'
-filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋10罗（92）.wav'
-#filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/旋律一（13）（98）.wav'
-#filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋1熙(90).wav'
-#filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋1谭（98）.wav'
-#filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋4谭（95）.wav'
-
-
-draw_plt(filename)
-plt.show()
 
 if __name__ == "__main__":
     #y, sr = load_and_trim('F:/项目/花城音乐项目/样式数据/ALL/旋律/1.31MP3/旋律1.100分.wav')
@@ -463,21 +615,27 @@ if __name__ == "__main__":
     #filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/旋律一（9）（100）.wav'
     #filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/旋律一（14）（95）.wav'
     filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋律五（3）（63）.wav'
-    filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/节奏/节奏一（4）（96）.wav'
-    filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋1录音4(78).wav'
-    filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋3王（80）.wav'
-    filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋4谭（95）.wav'
-    filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋4文(75).wav'
-    filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/旋律6.4(90).wav'
+    #filename = 'F:/项目/花城音乐项目/样式数据/2.27MP3/节奏/节奏一（4）（96）.wav'
+    #filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋3王（80）.wav'
+    #filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋4谭（95）.wav'
+    #filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋4文(75).wav'
+    #filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋8录音1(80).wav'
+    #filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋1.3(93).wav'
+    filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋3罗（80）.wav'
+    filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋律1_40312（95）.wav'
+    filename = 'e:/test_image/m1/A/旋律1_40312（95）.wav'
+    filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋3罗（80）.wav'
+    filename = 'F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/旋律四（4）（30）.wav'
 
-    draw_plt(filename)
+    plt, score = draw_plt(filename)
     plt.show()
+    plt.clf()
 
 
     dir_list = ['F:/项目/花城音乐项目/样式数据/3.06MP3/旋律/']
     #dir_list = ['F:/项目/花城音乐项目/样式数据/2.27MP3/旋律/']
-    # dir_list = ['e:/test_image/m1/A/']
-    #dir_list = []
+    #dir_list = ['e:/test_image/m1/D/']
+    dir_list = []
     total_accuracy = 0
     total_num = 0
     result_path = 'e:/test_image/n/'
@@ -495,7 +653,7 @@ if __name__ == "__main__":
             print(dir + filename)
             # plt = draw_start_end_time(dir + filename)
             # plt = draw_baseline_and_note_on_cqt(dir + filename, False)
-            plt = draw_plt(dir + filename)
+            plt, onset_score = draw_plt(dir + filename)
             # tmp = os.listdir(result_path)
 
             if filename.find("tune") > 0 or filename.find("add") > 0 or filename.find("shift") > 0:
@@ -521,5 +679,5 @@ if __name__ == "__main__":
             # result_path = result_path + grade + "/"
             # plt.savefig(result_path + filename + '.jpg', bbox_inches='tight', pad_inches=0)
             #grade = 'A'
-            plt.savefig(result_path + grade + "/" + filename + '.jpg', bbox_inches='tight', pad_inches=0)
+            plt.savefig(result_path + grade + "/" + filename + '-'+ str(onset_score) + '.jpg', bbox_inches='tight', pad_inches=0)
             plt.clf()
