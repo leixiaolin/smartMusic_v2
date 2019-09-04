c12d7252345c2755421064173103c1601219102e
leixiaolin <498900619@qq.com>
2019-09-04 10:30:34 +0800

    图片切换的保存路径

--- split_path_helper.py
+++ split_path_helper.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*-
-
+import os
 savepath = '/home/lei/bot-rating/split_pic/'
 savepath = 'E:/t/'  # 保存要测试的目录
 def get_split_pic_save_path():
+    path = os.path.abspath('.')
+    savepath = path + '/split_pic/'
     return savepath
