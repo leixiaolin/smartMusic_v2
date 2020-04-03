1bd43eb848d64eed5b53c7d3ef11ba952ee31bad
e <498900619@qq.com>
2020-04-03 18:07:54 +0800

    科大讯飞语音转文字

---
+++ xfyun/wav2pcm.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+import os
+import numpy as np
+import numpy as np
+def wav2pcm(wavfile):
+    str = "F:/ffmpeg/bin/ffmpeg -y -i " + wavfile + " -acodec pcm_s16le -f s16le -ac 1 -ar 16000 " + wavfile.split('.wav')[0] + ".pcm"
+    print(str)
+    # os.system("ffmpeg -i "+ m4a_path + m4a + " " + m4a_path + m4a.split('.')[0] + ".wav" )   # ffmpeg 可以用来处理视频和音频文件，可               #以设置一些参数，如采样频率等
+    os.system(str)
+if __name__ == "__main__":
+
+    wavfile = 'F:/项目/花城音乐项目/样式数据/20.04.01MP3/人声打分.wav'
+    wavfile = 'F:/项目/花城音乐项目/样式数据/12.05MP3/wav/旋律/B-6.wav'
+    wav2pcm(wavfile)
